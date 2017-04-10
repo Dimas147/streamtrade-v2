bca5897af941779c0f2f5f335a166bd3c9a8d4e1
Dimas147 <007bond147@mail.ru>
2017-04-10 12:11:40 +0300

    stick up adding

--- js/system/flexkit/other.js
+++ js/system/flexkit/other.js
@@ -1,16 +1,16 @@
 // Show button "Top" at the scrolling of documents
 var topBox = $('<div class="webPage-go-top"></div>').css({
     position  : 'fixed',
-    bottom    : '20px',
-    right     : '20px',
-    'z-index' : '555',
+    bottom    : '40px',
+    right     : '40px',
+    'z-index' : '110',
     'cursor'  : 'pointer',
     'display' : 'none'
 }).addClass('btn icon icon-arrow-up2');
 if(!device.type) {
     $('body').append(topBox);
     $(window).scroll(function(e) {
-        $(window).scrollTop() <= 300 ? $('.webPage-go-top').hide('slow') : $('.webPage-go-top').show('slow');
+        $(window).scrollTop() <= 300 ? $('.webPage-go-top').hide() : $('.webPage-go-top').show('slow');
     });
 }
 $(document).on('click', '.webPage-go-top', function(e) {
