30c4a164d8f14e3b63d4a008457a07d32daad98d
Dimas147 <007bond147@mail.ru>
2017-05-26 00:27:30 +0300

    parallax script changes

--- js/system/flexkit/3.parallax.js
+++ js/system/flexkit/3.parallax.js
@@ -1,10 +1,14 @@
 // Parallax(if you use imageonly for background image)
-(function () {
-	if (document.getElementsByClassName('parallax').length && document.getElementsByClassName('take-from-widget').length) {
-		var i = 0, href, listWhichTake = document.getElementsByClassName('take-from-widget');
-		for (; i < listWhichTake.length; i++) {
-			href = listWhichTake[i].childNodes[1].src;
-			listWhichTake[i].style.backgroundImage = 'url(' + href + ')';
-		}
-	}
-})();
+function _parallax() {
+    if (document.getElementsByClassName('parallax').length && document.getElementsByClassName('take-from-widget').length) {
+        var i = 0, href, listWhichTake = document.getElementsByClassName('take-from-widget');
+        for (; i < listWhichTake.length; i++) {
+            href = listWhichTake[i].childNodes[1].src;
+            listWhichTake[i].style.backgroundImage = 'url(' + href + ')';
+        }
+    }
+}
+_parallax();
+$(".show-more").click(function () {
+    _parallax();
+});
